dddfd06709de60e80721cede130c340b35053674
IGIRANEZA Joseph <igiranezajoseph50@gmail.com>
2026-02-22 15:45:07 +0200

    add API helper

---
+++ frontend/js/api.js
@@ -0,0 +1,22 @@
+const API_URL = 'http://localhost:5000/api';
+
+const getToken = () => localStorage.getItem('token');
+
+const apiFetch = async (endpoint, options = {}) => {
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${getToken()}`,
+      ...options.headers
+    }
+  });
+  return res.json();
+};
+
+const api = {
+  get: (endpoint) => apiFetch(endpoint),
+  post: (endpoint, body) => apiFetch(endpoint, { method: 'POST', body: JSON.stringify(body) }),
+  put: (endpoint, body) => apiFetch(endpoint, { method: 'PUT', body: JSON.stringify(body) }),
+  delete: (endpoint) => apiFetch(endpoint, { method: 'DELETE' })
+};
